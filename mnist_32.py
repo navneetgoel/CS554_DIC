d6b53b5642c0764b5f577ef29d53c75620a47780
navneetgoel <goel.navneet4@gmail.com>
2017-12-02 01:41:43 -0600

    Updated code for minor bugs

--- mnist_32.py
+++ mnist_32.py
@@ -6,33 +6,6 @@ mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
 #getting the start time of the program.
 startTime = datetime.datetime.now()
 
-##
- # returns pixels values and labels of the training data
- # @param  size:  number of images
- # @return x_train : pixel values of images
- # @return y_train : labels for corresponding images
-##
-
-def TRAIN_SIZE(size):
-	x_train = mnist.train.images[:size,:]
-	y_train = mnist.train.labels[:size,:]
-	return x_train, y_train
-
-
-##
- # returns pixels values and labels of the testing data
- # @param  size:  number of images
- # @return x_train : pixel values of images
- # @return y_train : labels for corresponding images
-##
-
-def TEST_SIZE(size):
-	x_test = mnist.test.images[:size,:]
-	y_test = mnist.test.labels[:size,:]
-	return x_test, y_test
-
-
-
 #Placeholder defined to hold the pixel values of the 28*28 pixel images and output label values#
 x = tf.placeholder(tf.float32, [None, 784])
 y = tf.placeholder(tf.float32, [None, 10])
@@ -70,10 +43,10 @@ cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=pr
 # Total Training images in dataset = 55000
 # Total Test images in dataset = 10000
 #
-x_train, y_train = TRAIN_SIZE(5500)
 x_test, y_test = TEST_SIZE(10000)
 LEARNING_RATE = 0.01
 TRAIN_STEPS = 10000
+BATCH_SIZE = 100
 
 # optimizing the values of weights and baises to reduce cost.
 training = tf.train.AdamOptimizer(LEARNING_RATE).minimize(cross_entropy)
@@ -84,18 +57,20 @@ accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 sess = tf.InteractiveSession()
 tf.global_variables_initializer().run()
 
-
+#
+# Training and testing the model 
+# Also calculating various metrics for the model
+# Accuracy, Elapsed Time, Cost
+#
 
 for i in range(TRAIN_STEPS):
-  batch_xs, batch_ys = mnist.train.next_batch(100)
-  sess.run(training, feed_dict={x: batch_xs, y: batch_ys})
+  x_train, y_train = mnist.train.next_batch(BATCH_SIZE)
+  sess.run(training, feed_dict={x: x_train, y: y_train})
   if i%1000 == 0:
-#         correct_prediction = tf.equal(tf.argmax(y,1), tf.argmax(y_,1))
-#         accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
-        values = sess.run(accuracy, feed_dict={x: mnist.test.images, y: mnist.test.labels})
-       	print('Accuracy :: ',values)
-        print('For :: ', i)
-        print('Time elasped :: ', datetime.datetime.now())
+    values = sess.run(accuracy, feed_dict={x: mnist.test.images, y: mnist.test.labels})
+    print('Accuracy :: ',values)
+    print('For :: ', i)
+    print('Time elasped :: ', datetime.datetime.now())
 
 
 tf = datetime.datetime.now()
